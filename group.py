a8cb6aaf2ebd58aacbd51d66c4d2a087c095f8d9
Sergei Parshenkov <288serega654@mail.ru>
2018-03-14 11:38:45 +0300

    Класс контактов сохранил в отдельном файле.

--- group.py
+++ group.py
@@ -4,11 +4,3 @@ class Group:
         self.name = name
         self.header = header
         self.footer = footer
-
-class Contact:
-
-    def __init__(self, firstname, middlename, lastname, nickname):
-        self.firstname = firstname
-        self.middlename = middlename
-        self.lastname = lastname
-        self.nickname = nickname
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
-from group import Contact
+from contact import Contact
 
 def is_alert_present(wd):
     try:
