ef343d04a02d992565eec604dac6290831ec1266
Sergei Parshenkov <288serega654@mail.ru>
2018-03-19 22:47:54 +0300

    Реализована общая для всех тестов фикстура в файле conftest.py.

--- fixture/group.py
+++ fixture/group.py
@@ -30,3 +30,14 @@ class GroupHelper:
     def open_groups_page(self):
             wd = self.app.wd
             wd.find_element_by_link_text("groups").click()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        wd.find_element_by_link_text("groups").click()
+        #select first group
+        wd.find_element_by_name("selected[]").click()
+        #submit delition
+        wd.find_element_by_name("delete").click()
+        #retern to groups page
+        self.retern_to_groups_page()
