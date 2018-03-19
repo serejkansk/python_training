0a13fe23cf47c0891637f79bd5f4ac70dc5d68a3
Sergei Parshenkov <288serega654@mail.ru>
2018-03-19 22:04:12 +0300

    Выделен класс для управления сессиями.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,12 +11,12 @@ def app(request):
     return fixture
 
 def test_add_contact(app):
-        app.login(username="admin", password="secret")
+        app.session.login(username="admin", password="secret")
         app.create_contact(Contact(firstname="svgasa", middlename="sagas", lastname="sbgasb", nickname="sah"))
-        app.logout()
+        app.session.logout()
 
 
 def test_add_empty_contact(app):
-        app.login(username="admin", password="secret")
+        app.session.login(username="admin", password="secret")
         app.create_contact(Contact(firstname="", middlename="", lastname="", nickname=""))
-        app.logout()
+        app.session.logout()
