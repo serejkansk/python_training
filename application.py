21e52210655358d429a5aee451fd0cc886180dd8
Sergei Parshenkov <288serega654@mail.ru>
2018-03-19 17:25:07 +0300

    Для групп и контактов использую один и тот же класс Application.

--- application.py
+++ application.py
@@ -7,7 +7,6 @@ class Application:
         self.wd.implicitly_wait(60)
 
     def logout(self):
-            # logout
             wd = self.wd
             wd.find_element_by_link_text("Logout").click()
 
@@ -33,6 +32,29 @@ class Application:
             # submit group creation
             wd.find_element_by_name("submit").click()
             self.retern_to_groups_page()
+
+    def create_contact(self, group):
+        wd = self.wd
+        self.open_add_new_page()
+        # fill contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(group.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(group.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(group.lastname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(group.nickname)
+        # submit contact creation
+        wd.find_element_by_name("submit").click()
+
+    def open_add_new_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("add new").click()
 
     def open_groups_page(self):
             wd = self.wd
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
 from contact import Contact
-from application_contact import Application
+from application import Application
 
 @pytest.fixture
 def app(request):
