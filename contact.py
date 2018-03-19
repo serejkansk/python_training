abc3146db16a7425625957db027fa23088bd0e27
Sergei Parshenkov <288serega654@mail.ru>
2018-03-19 20:48:06 +0300

    Созданы пакеты для разных зон ответственности.

--- contact.py
+++
@@ -1,7 +0,0 @@
-class Contact:
-
-    def __init__(self, firstname, middlename, lastname, nickname):
-        self.firstname = firstname
-        self.middlename = middlename
-        self.lastname = lastname
-        self.nickname = nickname
