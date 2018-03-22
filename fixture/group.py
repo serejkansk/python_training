0797904b5ae28ba7a33940f08a4366e4ef93f14f
Sergei Parshenkov <288serega654@mail.ru>
2018-03-22 21:19:10 +0300

    Добавил предусловие для удаления первой группы.

--- fixture/group.py
+++ fixture/group.py
@@ -63,3 +63,8 @@ class GroupHelper:
         wd.find_element_by_name("update").click()
         # retern to groups page
         self.retern_to_groups_page()
+
+    def count(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        return len(wd.find_elements_by_name("selected[]"))
