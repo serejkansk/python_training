184ad742cd12aaa14ef5f310a8bafc4ee9df5154
Sergei Parshenkov <288serega654@mail.ru>
2018-03-22 21:29:58 +0300

    Обеспечил выполнение предусловий в тестах для удаления и модификации для групп и контактов.

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,4 +2,6 @@
 from model.contact import Contact
 
 def test_modify_contact_firstname(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(lastname="tester"))
     app.contact.modify_first_contact(Contact(firstname="New firstname"))
