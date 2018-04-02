488dddaa6664d7adeccf80ba0ae5fc5288ffe287
Sergei Parshenkov <288serega654@mail.ru>
2018-04-02 22:57:50 +0300

    Начал задание 5.

--- model/contact.py
+++ model/contact.py
@@ -3,11 +3,16 @@ from sys import maxsize
 
 class Contact:
 
-    def __init__(self, firstname = None, middlename = None, lastname = None, nickname = None, id = None):
+    def __init__(self, firstname = None, middlename = None, lastname = None, nickname = None,\
+                 homephone = None, mobilephone = None, workphone = None, secondaryphone = None,  id = None):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
         self.nickname = nickname
+        self.homephone = homephone
+        self.mobilephone = mobilephone
+        self.workphone = workphone
+        self.secondaryphone = secondaryphone
         self.id = id
 
     def __repr__(self):
