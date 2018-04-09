e568ee7d9938988176b42e5fd636ee9ddab9e525
Sergei Parshenkov <288serega654@mail.ru>
2018-04-09 23:40:51 +0300

    Задание 18.

---
+++ generator/contact.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from model.contact import Contact
+import random
+import string
+import os.path
+import json
+import getopt
+import sys
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of contacts", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 2
+f = "data/contacts.json"
+
+for o, a in opts:
+    if o =="-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
+def random_string(prefix, maxlen):#генератор случайных строк
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10#символы, которые собираемся использовать в случайно сгенерированной строке
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])#выбираем символ из заданной стркои случайным образом (многократно), будет сгенерированна случайная длина не превышающая максимальную
+
+testdata = [Contact(firstname="", middlename="", lastname="", nickname="")] + [
+    Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 15), lastname=random_string("lastname", 20),
+            nickname=random_string("nickname", 10))
+    for i in range(n)
+    ]
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)#определили путь к файлу
+
+with open(file, "w") as out:
+    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
