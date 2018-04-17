dde609e7c525124cee23cfd3747ca28319d45931
Sergei Parshenkov <288serega654@mail.ru>
2018-04-17 10:11:22 +0300

    7_4что-то заработало.

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -9,8 +9,8 @@ def test_delete_some_group(app, db, check_ui):
     group = random.choice(old_groups)
     app.group.delete_group_by_id(group.id)
     new_groups = db.get_group_list()
-#    assert len(old_groups) - 1 == len(new_groups)
+    assert len(old_groups) - 1 == len(new_groups)
     old_groups.remove(group)
-#    assert old_groups == new_groups
+    assert old_groups == new_groups
     if check_ui:
         assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list, key=Group.id_or_max)
