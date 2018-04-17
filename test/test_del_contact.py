e576061c2e2d58ce29a0efb0ab6015ec503c3bfd
Sergei Parshenkov <288serega654@mail.ru>
2018-04-17 13:37:53 +0300

    7_5(добавление, удаление контактов и групп).

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -2,7 +2,7 @@
 from model.contact import Contact
 import random
 
-def test_delete_some_contact(app, db):
+def test_delete_some_contact(app, db, check_ui):
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(lastname="tester"))
     old_contacts = db.get_contact_list()
@@ -12,3 +12,5 @@ def test_delete_some_contact(app, db):
     assert len(old_contacts) - 1 == len(new_contacts)
     old_contacts.remove(contact)
     assert old_contacts == new_contacts
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
