4afc22800509c946e7965be2a94e42b2bd28aede
Sergei Parshenkov <288serega654@mail.ru>
2018-04-17 13:52:12 +0300

    7_6 сделал check_db_connection для контактов и групп фикстурой Db.

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,15 +1,15 @@
-import pymysql.cursors
+#import pymysql.cursors
 from fixture.db import DbFixture
 
-connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+#connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
 
-try:
-    cursor = connection.cursor()
-    cursor.execute("select * from group_list")
-    for row in cursor.fetchall():
-        print(row)
-finally:
-    connection.close()
+#try:
+#    cursor = connection.cursor()
+#    cursor.execute("select * from group_list")
+#    for row in cursor.fetchall():
+#        print(row)
+#finally:
+#    connection.close()
 
 db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
@@ -18,14 +18,10 @@ try:
     for contact in contacts:
         print(contact)
     print(len(contacts))
+    groups = db.get_group_list()
+    for group in groups:
+        print(group)
+    print(len(groups))
 finally:
     db.destroy()
-
-#try:
-#    groups = db.get_group_list()
-#    for group in groups:
-#        print(group)
-#    print(len(groups))
-#finally:
-#    db.destroy()
 
